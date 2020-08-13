c4da056b18c66391ba47507e47437e1d3c200e0a
Shubham Rane <official.shubhamrane@gmail.com>
2020-08-13 11:14:17 -0400

    Base commits

---
+++ script.js
@@ -0,0 +1,24 @@
+/*******************************
+ * console.log, prompt, alert
+var firstName = "Shubham";
+var age = 22;
+
+alert("Your name is "+ firstName + " and your age is "+age+".");
+alert("Can you tell us your last name?");
+var lastName = prompt("Enter last name here: ");
+console.log("My full name is "+ firstName +" "+ lastName);
+
+if (age > 21) {
+    var consent = prompt("Are you ready for marriage yet? (Yes/No)");
+}
+if(consent == "Yes"){
+    alert("We are posting you on matremonial site.");
+}else if(consent == "No"){
+    alert("Then die single.")
+}else{
+    alert("INVALID INPUTE");
+}
+*/
+
+
+
